0351060d186f4080e3b700b040aab7d4b2a11be4
madlajos <madlajos96@gmail.com>
2017-07-02 12:23:52 +0200

    D számolás átírva, arduino support előtt
    Itt még működik

--- src/main/Beolvas.java
+++ src/main/Beolvas.java
@@ -27,22 +27,15 @@ public class Beolvas {
 			String[] b = line.split(splitBy);
 			array = Arrays.stream(b).mapToDouble(Double::parseDouble).toArray();
 
-			//System.out.println(Arrays.toString(array));
-			//System.out.println(array[1]);
-
-
-
-			d = Math.sqrt(4*array[1]/Math.PI);
+			d = (array[4] + array[8])/2 ;
 			dsum += d;
 			ARsum += array[10];
 			count ++;
-			// V-t m�shogy kell sz�molni hall�
+			
 			double V = 4.0*Math.PI*Math.pow(d/2, 3)/3;
 			Vsum += V;
 			ip.addItem(d);
-			
 		}
-
 
 		br.close();
 
@@ -52,8 +45,7 @@ public class Beolvas {
 		ta1.appendText(String.format("\n%.1f", davg));
 		ta2.appendText(String.format("\n%.2f", ARavg));
 
-
-		//Osszes.arff-et üressé teszi
+		//Clear Osszes.arff
 		PrintWriter pw = new PrintWriter(filePath);
 		pw.close();
 	}
